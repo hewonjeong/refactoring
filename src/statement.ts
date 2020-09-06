e72b2a7bfad05983028e414c0a06601aa1d480c7
Hewon Jeong <hewon.jng@gmail.com>
2020-09-06 22:58:39 +0900

    feat: implement htmlStatement

--- src/statement.ts
+++ src/statement.ts
@@ -1,14 +1,8 @@
 import createStatementData from './createStatementData'
 import { Invoice, Plays, StatementData } from '../types'
 
-export default function statement(invoice: Invoice, plays: Plays) {
+export function statement(invoice: Invoice, plays: Plays) {
   return renderPlainText(createStatementData(invoice, plays))
-}
-
-
-function renderHtml(data: StatementData) {
-  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`
-  result += '<table>\n'
 }
 
 function renderPlainText(data: StatementData) {
@@ -21,14 +15,34 @@ function renderPlainText(data: StatementData) {
 
   result += `Amount owed is ${usd(data.totalAmount)}\n`
   result += `You earned ${data.totalVolumeCredits} credits\n`
+  return result
+}
+
+export function htmlStatement(invoice: Invoice, plays: Plays) {
+  return renderHtml(createStatementData(invoice, plays))
+}
+
+function renderHtml(data: StatementData) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`
+  result += '<table>\n'
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>'
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`
+    result += `<td>${usd(perf.amount)}</td></tr>\n`
+  }
+  result += '</table>\n'
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`
 
   return result
-
-  function usd(number: number) {
-    return new Intl.NumberFormat('en-us', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(number / 100)
-  }
 }
+
+function usd(number: number) {
+  return new Intl.NumberFormat('en-us', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(number / 100)
+}
+
+export default statement
