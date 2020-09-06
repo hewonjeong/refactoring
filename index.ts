bbb6f170a170196960fd9e7f29418367db7c1c55
Hewon Jeong <hewon.jng@gmail.com>
2020-09-06 21:44:38 +0900

    refactor: inline totalAmount variable

--- index.ts
+++ index.ts
@@ -55,29 +55,28 @@ export function statement(invoice: Invoice, plays: Plays) {
     return result
   }
 
-  function appleSauce() {
-    let totalAmount = 0
+  function totalAmount() {
+    let result = 0
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf)
+      result += amountFor(perf)
     }
-    return totalAmount
+    return result
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0
+    let result = 0
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf)
+      result += volumeCreditsFor(perf)
     }
-    return volumeCredits
+    return result
   }
 
   let result = `청구 내역 (고객명: ${invoice.customer})\n`
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience }석)\n` // prettier-ignore
   }
-  let totalAmount = appleSauce()
 
-  result += `총액: ${usd(totalAmount)}\n`
+  result += `총액: ${usd(totalAmount())}\n`
   result += `적립 포인트: ${totalVolumeCredits()}점\n`
   return result
 }
