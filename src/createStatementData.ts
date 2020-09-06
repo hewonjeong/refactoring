67e0bd21e6d83adf4daf8932c76be77b76613c15
Hewon Jeong <hewon.jng@gmail.com>
2020-09-06 23:47:54 +0900

    refactor: override calculate amount logic in ComedyCalculator

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -63,23 +63,8 @@ class PerformanceCalculator {
     this.play = play
   }
 
-  get amount() {
-    let result = 0
-    switch (this.play.type) {
-      case 'tragedy':
-        throw 'bad thing'
-      case 'comedy':
-        result = 30000
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20)
-        }
-        result += 300 * this.performance.audience
-        break
-
-      default:
-        throw new Error(`unknown type: ${this.play.type}`)
-    }
-    return result
+  get amount(): number {
+    throw new Error('subclass responsibility')
   }
 
   get volumeCredits() {
@@ -104,4 +89,13 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20)
+    }
+    result += 300 * this.performance.audience
+    return result
+  }
+}
