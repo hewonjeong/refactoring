16e0acd6dce11a502ced8b881f71b65f3ccf9b43
Hewon Jeong <hewon.jng@gmail.com>
2020-09-06 22:55:42 +0900

    feat: render with English

--- src/statement.test.ts
+++ src/statement.test.ts
@@ -4,12 +4,12 @@ import plays from './plays.json'
 
 describe('statement', () => {
   it('returns output of statement', () => {
-    const expected = `청구 내역 (고객명: BigCo)
-  Hamlet: $650.00 (55석)
-  As You Like It: $580.00 (35석)
-  Othello: $500.00 (40석)
-총액: $1,730.00
-적립 포인트: 47점
+    const expected = `Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $580.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
 `
 
     const result = statement(invoices[0], plays)
--- src/statement.ts
+++ src/statement.ts
@@ -5,14 +5,22 @@ export default function statement(invoice: Invoice, plays: Plays) {
   return renderPlainText(createStatementData(invoice, plays))
 }
 
+
+function renderHtml(data: StatementData) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`
+  result += '<table>\n'
+}
+
 function renderPlainText(data: StatementData) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`
+  let result = `Statement for ${data.customer}\n`
   for (let perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n` // prettier-ignore
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`
   }
 
-  result += `총액: ${usd(data.totalAmount)}\n`
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`
+  result += `Amount owed is ${usd(data.totalAmount)}\n`
+  result += `You earned ${data.totalVolumeCredits} credits\n`
 
   return result
 
