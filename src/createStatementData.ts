609ed5c82c0cbcb8bc2c51e548a582ae3b0c65e2
Hewon Jeong <hewon.jng@gmail.com>
2020-09-06 23:32:41 +0900

    refactor: calculate amount with moving function

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -25,37 +25,13 @@ export default function createStatementData(
     )
     const result = Object.assign({} as PerformanceData, performance)
     result.play = calculator.play
-    result.amount = amountFor(result)
+    result.amount = calculator.amount
     result.volumeCredits = volumeCreditsFor(result)
     return result
   }
 
   function playFor(performance: Performance) {
     return plays[performance.playID]
-  }
-
-  function amountFor(performance: PerformanceData) {
-    let result = 0
-    switch (performance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30)
-        }
-        break
-
-      case 'comedy':
-        result = 30000
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20)
-        }
-        result += 300 * performance.audience
-        break
-
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`)
-    }
-    return result
   }
 
   function totalAmount(data: StatementData) {
@@ -85,5 +61,29 @@ class PerformanceCalculator {
   constructor(performance: Performance, play: Play) {
     this.performance = performance
     this.play = play
+  }
+
+  get amount() {
+    let result = 0
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
+        }
+        break
+
+      case 'comedy':
+        result = 30000
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20)
+        }
+        result += 300 * this.performance.audience
+        break
+
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`)
+    }
+    return result
   }
 }
